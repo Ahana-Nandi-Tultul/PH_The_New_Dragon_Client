178dceccb63b5ffc91d17fec09efd8f3f0d85f92
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-08-10 12:37:43 +0600

    add Header

---
+++ README.md
@@ -0,0 +1,7 @@
+### The NewsDragon
+
+# Here I have used:
+1. React Bootstrap for layout and front-end making
+2. Firebase for authentication.
+3. Moment js for showing current date.
+4. React Fast Marquee for showing latest news.
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import logo from '../../../assets/logo.png'
+import Marquee from "react-fast-marquee";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import moment from 'moment/moment';
+
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <div className='text-center'>
+                <img src={logo} alt="" />
+                <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
+                <p><small>{moment().format("dddd, MMMM D, YYYY")}</small></p>
+            </div>
+            <div className='d-flex'>
+                <Button variant="danger">Latest</Button>
+                <Marquee speed={100}>
+                    I can be a React component, multiple React components, or just some text.
+                </Marquee>
+            </div>
+        </Container>
     );
 };
 
